bdff470f38b6841e7b38e094d0aa9ddef22bfd53
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2022-01-19 23:58:44 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,56 +1,37 @@
+const transpose = function(matrix) {
+    // Put your solution here
+    let array = [];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (i === 0) {
+          array[j] = [];
+        }
+        array[j][i] = matrix[i][j];
+      }
+    }
+    return array;
+  };
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    if (letters.length === 0) {
+        return false;
+    }
+    const len = word.length;
+    let count = 0;
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
         if (l.includes(word)) {
-            return true
+            return true;
         }
     }
-
-    for (const line of letters) {
-        line.reverse();
-    }
-
-    const reverseHorizontalJoin = letters.map(ls => ls.join(''))
-    for (l of reverseHorizontalJoin) {
-        if (l.includes(word)) {
-            return true
-        }
-    }
-
     const vertical = transpose(letters);
-    const verticalJoin = vertical.map(ls => ls.join(''))
+    const verticalJoin = vertical.map(ls => ls.join(''));
+    //console.log(verticalJoin);
     for (l of verticalJoin) {
         if (l.includes(word)) {
-            return true
+            return true;
         }
     }
-
-
-    for (const line of vertical) {
-        line.reverse();
-    }
-    const reverseVerticalJoin = vertical.map(ls => ls.join(''))
-    for (l of reverseVerticalJoin) {
-        if (l.includes(word)) {
-            return true
-        }
-    }
-
     return false;
-}
-
-
-
-const transpose = function(matrix) {
-    const newMatrix = [];
-    for (let y = 0; y < matrix[0].length; y++) {
-    console.log(matrix[y]) // to remove
-    newMatrix[y] = [];
-    for (let x = 0; x < matrix.length; x++) {
-        newMatrix[y].push(matrix[x][y]);
-    }
-    }
-    return newMatrix;
 };
-
 module.exports = wordSearch
